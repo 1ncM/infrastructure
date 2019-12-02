4c265ea9a9b9a6376558918b0c9024b10f07cb90
AChuprov <microposts@gmail.com>
2019-12-02 05:55:02 -0800

    Added action index,show,new,create for switch

--- app/controllers/switches_controller.rb
+++ app/controllers/switches_controller.rb
@@ -1,9 +1,29 @@
 class SwitchesController < ApplicationController
 	def index
-		
+	  @switches = Switch.all
 	end
 
 	def show
+		@switch = Switch.find(params[:id])
+	end
+
+	def new
 		
+	end
+
+	def create
+		@switch = Switch.create(switch_params)
+
+		if @switch.save
+			redirect_to @switch
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def switch_params
+		params.require(:switch).permit(:name, :model)
 	end
 end
